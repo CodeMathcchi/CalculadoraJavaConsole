9a5893e56f9e511a794ce3957496c0577583489c
Matheus Santos <santos.matheusgr@gmail.com>
2020-10-23 11:26:44 -0300

    Separação em métodos

--- src/Program.java
+++ src/Program.java
@@ -6,45 +6,62 @@ public class Program {
 		Scanner scan = new Scanner(System.in);
 		int continuar = 1;
 		
-		int numero1;
-		int numero2;
+		final String MENSAGEM_RESULTADO =  "Resultado: ";
+
+		double numero1;
+		double numero2;
 		int operacao;
 		
 		do {
-			System.out.println("Escolha a operação desejada:");
-			System.out.println("1 - Adição");
-			System.out.println("2 - Subtração");
-			System.out.println("3 - Multiplicação");
-			System.out.println("4 - Divisão");
+			System.out.println("Escolha a operacao desejada:");
+			System.out.println("1 - Adicao");
+			System.out.println("2 - Subtracao");
+			System.out.println("3 - Multiplicacao");
+			System.out.println("4 - Divisao");
 			operacao = scan.nextInt();
 			
-			System.out.println("Digite o primeiro número:");
+			System.out.println("Digite o primeiro numero:");
 			numero1 = scan.nextInt();
-			System.out.println("Digite o segundo número:");
+			System.out.println("Digite o segundo numero:");
 			numero2 = scan.nextInt();
 			
 			switch(operacao) {
 			case 1:
-				System.out.println("Resultado: " + (numero1 + numero2));
+				System.out.println(MENSAGEM_RESULTADO + Adicao(numero1, numero2));
 				break;
 			case 2:
-				System.out.println("Resultado: " + (numero1 - numero2));
+				System.out.println(MENSAGEM_RESULTADO + Subtracao(numero1, numero2));
 				break;
 			case 3:
-				System.out.println("Resultado: " + (numero1 * numero2));
+				System.out.println(MENSAGEM_RESULTADO + Multiplicacao(numero1, numero2));
 				break;
 			case 4:
-				System.out.println("Resultado: " + (numero1 / numero2));
+				System.out.println(MENSAGEM_RESULTADO + Divisao(numero1, numero2));
 				break;
 			default:
-				System.out.println("Operação inválida");
+				System.out.println("Operacao invalida");
 			}
 			
-			System.out.println("Deseja fazer outra operação?");
+			System.out.println("Deseja fazer outra operacao?");
 			System.out.println("1 - Sim");
-			System.out.println("0 - Não");
+			System.out.println("0 - Nao");
 			continuar = scan.nextInt();
 		}while(continuar == 1);
 	}
 
+	public double Adicao(double num1, double num2){
+		return num1 + num2;
+	}
+
+	public double Subtracao(double num1, double num2){
+		return num1 - num2;
+	}
+
+	public double Multiplicacao(double num1, double num2){
+		return num1 * num2;
+	}
+
+	public double Divisao(double num1, double num2){
+		return num1 / num2;
+	}
 }
